ac3fa3fc68a8facfb0284a04e702b247627964f1
Andreas Kring <andreas@magenta.dk>
2017-01-11 16:15:13 +0100

    Added setup script for the DB

--- db/setup_db.sql
+++ db/setup_db.sql
@@ -1,28 +1,27 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.5-10.0.20-MariaDB)
-# Database: exm
-# Generation Time: 2016-08-26 11:14:35 +0000
-# ************************************************************
-
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: exm
+-- ------------------------------------------------------
+-- Server version	5.5.53-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `Mappings`
+--
 
-# Dump of table Mappings
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `Mappings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Mappings` (
   `name` varchar(25) NOT NULL DEFAULT '',
   `sysPath` varchar(100) NOT NULL DEFAULT '',
@@ -31,12 +30,15 @@ CREATE TABLE `Mappings` (
   `realFileName` varchar(75) NOT NULL DEFAULT '',
   PRIMARY KEY (`name`,`sysPath`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `Profiles`
+--
 
-
-# Dump of table Profiles
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `Profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Profiles` (
   `name` varchar(50) NOT NULL DEFAULT '',
   `url` varchar(255) DEFAULT NULL,
@@ -44,25 +46,21 @@ CREATE TABLE `Profiles` (
   `password` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `Repositories`
+--
 
-
-# Dump of table Repositories
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `Repositories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Repositories` (
   `name` varchar(50) NOT NULL DEFAULT '',
   `repositoryName` varchar(70) NOT NULL DEFAULT '',
   PRIMARY KEY (`name`,`repositoryName`),
   CONSTRAINT `repositories_ibfk_1` FOREIGN KEY (`name`) REFERENCES `Profiles` (`name`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Dump completed on 2017-01-11 16:09:37
